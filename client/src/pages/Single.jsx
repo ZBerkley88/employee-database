8e2abedd323e29da7cbb3483c5771894f75c6a9c
Zack <zachary.berkley@gmail.com>
2023-06-06 08:56:42 -0400

    styled new post page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu"
 
 const Single = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/17082890/pexels-photo-17082890/free-photo-of-eye-of-the-valley.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
 
-export default Single
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/8206766/pexels-photo-8206766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+
+          <div className="info">
+            <span>Zack</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <button className="editBtn">Edit</button>
+            </Link>
+            <Link>
+              <button className="deleteBtn">Delete</button>
+            </Link>
+          </div>
+        </div>
+
+        <h1>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae,
+          magnam?
+        </h1>
+        <p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sit
+            deleniti cum quod odit quis fugit, nam recusandae error odio
+            temporibus, expedita ex! Quia rerum, distinctio natus quidem
+            doloremque quod.
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            nostrum harum quo! Quidem facilis, fuga eius officia nemo mollitia
+            vero voluptatem similique quae voluptates dolorum dicta perferendis
+            qui nisi natus!
+          </p>
+        </p>
+      </div>
+      <Menu />
+    </div>
+  );
+};
+
+export default Single;
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default Write
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+
+        <div className="item">
+          <h1>Category</h1>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="cat1" id="cat1" />
+            <label htmlFor="cat1">Cat 1</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="cat2" id="cat2" />
+            <label htmlFor="cat2">Cat 2</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="cat3" id="cat3" />
+            <label htmlFor="cat3">Cat 3</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="cat4" id="cat4" />
+            <label htmlFor="cat4">Cat 4</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Write;
